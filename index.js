ad081149d6d5c5a9fbff7f2f20b5a11a242eee7a
bosswt <boss.wt13@gmail.com>
2021-04-07 15:36:20 +0700

    ⚡️ Done(DFS)

--- index.js
+++ index.js
@@ -1,98 +1,53 @@
 const axios = require('axios');
 
+const result = {};
+result['transactions'] = [];
+result['balances'] = {};
+const checkedWallet = [];
+
 const get = async (url) => {
 	try {
 		const res = await axios.get(url);
-		setTimeout(() => {}, 500);
+		setTimeout(() => {}, 100);
 		return res;
 	} catch (e) {
 		console.error(e);
 	}
 };
 
-const url =
-	'https://api-ropsten.etherscan.io/api?module=account&action=tokentx&address=0xEcA19B1a87442b0c25801B809bf567A6ca87B1da&startblock=0&endblock=999999999&sort=asc&apikey=K7ST5DC6VP2Z5ZVWWD1IB3JDB5AHIEV274';
-
-let gobindAddress = '0xEcA19B1a87442b0c25801B809bf567A6ca87B1da';
-gobindAddress = gobindAddress.toLowerCase();
-
-const checkedWallet = [];
-checkedWallet.push(gobindAddress);
-
 const tracking = async (address, result) => {
 	const url = `https://api-ropsten.etherscan.io/api?module=account&action=tokentx&address=${address}&startblock=0&endblock=999999999&sort=asc&apikey=K7ST5DC6VP2Z5ZVWWD1IB3JDB5AHIEV274`;
 	const response = await get(url);
 	const data = response.data;
 	const transactions = data.result;
-	const filtered = [];
-	checkedWallet.push(address);
 	for (const t of transactions) {
-		if (t.from === address && t.tokenSymbol === 'BKTC') filtered.push(t);
-		if (t.to === address && t.tokenSymbol === 'BKTC')
+		if (t.from === address && t.tokenSymbol === 'BKTC') {
+			result['transactions'].push({
+				tx: t.hash,
+				from: t.from,
+				to: t.to,
+				amount: t.value / Math.pow(10, 18)
+			});
+			result['balances'][address] -= t.value / Math.pow(10, 18);
+			if (!checkedWallet.includes(t.to)) {
+				checkedWallet.push(t.to);
+				result['balances'][t.to] = 0;
+				await tracking(t.to, result);
+			}
+		} else if (t.to === address && t.tokenSymbol === 'BKTC') {
 			result['balances'][address] += t.value / Math.pow(10, 18);
-	}
-	const newWallet = [];
-	filtered.forEach((t) => {
-		result['transactions'].push({
-			Tx: t.hash,
-			from: t.from,
-			to: t.to,
-			Amount: t.value / Math.pow(10, 18)
-		});
-		const walletAddress = t.to;
-		if (!checkedWallet.includes(walletAddress))
-			newWallet.push(walletAddress);
-		if (!result['balances'][walletAddress]) {
-			result['balances'][address] -= t.value / Math.pow(10, 18);
-			result['balances'][walletAddress] = t.value / Math.pow(10, 18);
-		} else {
-			result['balances'][address] -= t.value / Math.pow(10, 18);
-			result['balances'][walletAddress] += t.value / Math.pow(10, 18);
 		}
-	});
-	// console.log(newWallet);
-	for (const t of newWallet) await tracking(t, result);
+	}
 };
 
 const main = async () => {
-	const response = await get(url);
-	const data = response.data;
-	const transactions = data.result;
-	const result = {};
-
-	result['transactions'] = [];
-	result['balances'] = {};
-	const filtered = [];
+	let gobindAddress = '0xEcA19B1a87442b0c25801B809bf567A6ca87B1da';
+	gobindAddress = gobindAddress.toLowerCase();
 	result['balances'][gobindAddress] = 0;
-	for (const t of transactions) {
-		if (t.from === gobindAddress && t.tokenSymbol === 'BKTC')
-			filtered.push(t);
-		if (t.to === gobindAddress && t.tokenSymbol === 'BKTC')
-			result['balances'][gobindAddress] += t.value / Math.pow(10, 18);
-	}
-	filtered.forEach((t) => {
-		result['transactions'].push({
-			Tx: t.hash,
-			from: t.from,
-			to: t.to,
-			Amount: t.value / Math.pow(10, 18)
-		});
-		const walletAddress = t.to;
-		if (!result['balances'][walletAddress]) {
-			result['balances'][gobindAddress] -= t.value / Math.pow(10, 18);
-			result['balances'][walletAddress] = t.value / Math.pow(10, 18);
-		} else {
-			result['balances'][gobindAddress] -= t.value / Math.pow(10, 18);
-			result['balances'][walletAddress] += t.value / Math.pow(10, 18);
-		}
-	});
-	// console.log(result);
-	for (const t of result['transactions']) {
-		const toAddress = t.to;
-		await tracking(toAddress, result);
-	}
+	checkedWallet.push(gobindAddress);
+	await tracking(gobindAddress, result);
 	console.log(result);
-	// console.log(checkedWallet);
+	console.log(result['transactions'].length);
 };
 
 main();
